df30128a9d762f88ec52b2eaaae597f6f1883ce1
Jason Laster <jason.laster.11@gmail.com>
2019-09-06 17:24:33 -0700

    switch to driving

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const images = ["cake", "house", "vase", "woman"];
-let index = 0;
-setInterval(() => {
-  const imageEls = document.querySelectorAll(".image");
+// const images = ["cake", "house", "vase", "woman"];
+// let index = 0;
+// setInterval(() => {
+//   const imageEls = document.querySelectorAll(".image");
 
-  imageEls.forEach(imageEl => {
-    imageEl.style.display = `none`;
-  });
+//   imageEls.forEach(imageEl => {
+//     imageEl.style.display = `none`;
+//   });
 
-  index = (index + 1) % 4;
-  const imageEl = document.querySelector("." + images[index]);
-  imageEl.style.display = `block`;
-}, 10000);
+//   index = (index + 1) % 4;
+//   const imageEl = document.querySelector("." + images[index]);
+//   imageEl.style.display = `block`;
+// }, 10000);
